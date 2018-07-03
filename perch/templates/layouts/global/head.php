5f6c001f68e073272e91b356a730907646fde6f6
Rick Chadwick <rick@madeinbeta.co.uk>
2018-07-03 12:55:50 +0100

    launch state

--- perch/templates/layouts/global/head.php
+++ perch/templates/layouts/global/head.php
@@ -57,15 +57,15 @@
 			<meta property="og:site_name" content="Dot York" />
 			<meta property="og:url" content="http://dotyork.com" />
 			<meta property="og:title" content="Dot York" />
-			<meta property="og:description" content="The Digital Conference for Curious Minds. Returning Autumn 2018." />
+			<meta property="og:description" content="The Conference for Curious Minds. Returning for its fifth year on Thursday 4th October 2018." />
 			<meta property="og:image" content="http://dotyork.com/assets/img/social.jpg" />
 
 			<meta name="twitter:card" content="summary_large_image">
 			<meta name="twitter:site" content="@dot_york">
 			<meta name="twitter:creator" content="@rickary">
 			<meta name="twitter:title" content="Dot York">
-			<meta name="twitter:description" content="The Digital Conference for Curious Minds. Returning Autumn 2018.">
-			<meta name="twitter:image" content="http://dotyork.com/assets/img/social_twitter.jpg">
+			<meta name="twitter:description" content="The Conference for Curious Minds. Returning for its fifth year on Thursday 4th October 2018.">
+			<meta name="twitter:image" content="http://dotyork.com/assets/img/social.jpg">
 	<?php	} ?>
 	
 	<meta property="fb:app_id" content="2389015207990323"/>
